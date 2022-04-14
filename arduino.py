347c5a9df3ec67109f1bbe067e4911c44d194e2c
JohnKoch02 <johnnykoch02@gmail.com>
2022-04-14 12:55:58 -0400

    Arduino Python Files

---
+++ arduino.py
@@ -0,0 +1,35 @@
+HIGH = 1
+LOW = 0
+INPUT = 0
+OUTPUT = 1
+PI = 3.1415926535897932384626433832795
+def radians(deg):
+    return deg * PI / 180
+def degrees(rad):
+    return rad * 180 / PI
+def pinMode(pin, mode):
+    print(pin, mode)
+def digitalWrite(pin, val):
+    print(pin, val)
+def digitalRead(pin):
+    pass
+def analogRead(pin):
+    pass
+def analogWrite(pin, val):
+    print(pin, val)
+def pulseIn(pin, state, timeout):
+    pass
+
+class Serial(object):
+    def __new__(cls):
+        if not hasattr(cls,'instance'):
+            cls.instance = super(Serial, cls).__new__(cls)
+        return cls.instance
+    def begin(self, baudrate):
+        pass
+    def print(self, data):
+        pass
+    def println(self, data):
+        pass
+
+
---
+++ sketch.py
@@ -0,0 +1,10 @@
+from arduino import *
+
+'''Arudino Sketch'''
+
+
+def setup():
+    pass
+
+def loop():
+    pass
